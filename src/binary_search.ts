07b44e0b177aa939b63f65d4ae0ba53ea2e9497d
dlottermann <diegolottermann@gmail.com>
2024-05-28 17:58:01 -0300

    apply linter

--- src/binary_search.ts
+++ src/binary_search.ts
@@ -1,4 +1,3 @@
-
 function binary_search<T>(list: T[], item: T): number | null {
   let low: number = 0;
   let high: number = list.length - 1;
@@ -6,9 +5,11 @@ function binary_search<T>(list: T[], item: T): number | null {
   while (low <= high) {
     let mid: number = Math.floor((low + high) / 2);
     let guess: T = list[mid];
+
     if (guess === item) {
       return mid;
     }
+
     if (guess > item) {
       high = mid - 1;
     } else {
